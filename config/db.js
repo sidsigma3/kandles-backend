8883fd1120be1dd5d117d6ba2ae48774f4e44290
SidSigmaX <sidsigma3@gmail.com>
2024-09-14 16:51:27 +0530

    chnages

--- config/db.js
+++ config/db.js
@@ -1,9 +1,24 @@
-const mysql = require('mysql')
+require('dotenv').config();
+const mysql = require('mysql');
+
+// Use the Clever Cloud environment variables to configure the connection
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "",
-database:"login"
-})
+  host: 'bjbjotkpn4piwqplzpwn-mysql.services.clever-cloud.com',
+  user:'unr1tnyago7kvkrv',
+  password:'4jkun8UayxYkgHocyj9Y',
+  database: 'bjbjotkpn4piwqplzpwn',
+  port: 3306
+});
+
+
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the MySQL database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database');
+});
 
 module.exports = db;
